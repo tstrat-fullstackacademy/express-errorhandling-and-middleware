cddd88ac70ceb46b70a250efda4502fdab588a11
Travis Stratton <t.strat7@gmail.com>
2020-01-24 11:15:08 -0600

    adding extra notes to routes

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+// usually the first routes will be '/' just so they are clearly visible
 router.get('/', (req, res, next) => {
   res.status(200).send('Here is your data');
 });
 
 router.get('/bad', (req, res, next) => {
+  // the catch block will always catch the first error to happen and will not even run the rest of the try block
   try {
     throw Error('my first error');
     throw Error('my second error');
     throw Error('my third error');
   } catch (error) {
+    // calling next with an error will send it to the next available error-handling route
+    // express has a default one, but we can write our own
     next(error);
   }
 });
 
+// Export the entire router
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// We will only get TO this route if the checkLogin middlware calls "next()"
 router.get('/', (req, res, next) => {
   res.status(200).send('You are logged in so you can see this, right?');
 });
